5bb29988aafdeb0d72b38e53dae5e4c865912c42
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-09-25 06:46:43 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,37 +1,34 @@
 package list_04_practice;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-    	Object s = new Hero();
-    	print((Hero)s,3);
 
-		ArrayList list = new ArrayList(); /*この書き方なら複数の型を一つのlistに入れることが可能*/
-    	list.add(1);
-    	list.add(2);
-    	list.add(3);
-    	list.add(4);
-    	list.add("a");
-    	list.add(s);
-    	for (Object o : list) {
-			System.out.println(o);
-		}
-    	System.out.println(list.size());
-    }
 
-    public static void print(Hero a,int b) {
-    	for (int i = 0; i < (Integer)b; i++) {
-    		System.out.println(a);
-    	}
+    	String q = "777";
+    	int qq = Integer.parseInt(q);
+    	System.out.println(qq);
+//    	Sample num = new Sample(1);
+    	Sample str = new Sample("1222");
+//    	System.out.println(num.o);
+    	System.out.println(str.o);
+//    	String ss = String.valueOf(num);
+    	String aa = String.valueOf(str);
+    	int i = Integer.parseInt(aa);
+//    	System.out.println("ss="+ss);
+    	System.out.println("aa="+i);
     }
+//    これだと文字から数値への変換が例外になる。
 }
-class Hero{
-	String name;
+class Sample{
+	Object o;
+	public Sample(Object o) {
+		this.o=o;
+	}
+//	@Override
+	public int toInsst() {
+    	String aa = String.valueOf(o);
 
-	@Override
-	public String toString() {
-		return "Hero [name=" + name + "]";
+		return Integer.parseInt(aa);
 	}
 
 }
